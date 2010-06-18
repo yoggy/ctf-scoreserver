0c4a64afa21a613305da36c0038cd98e50af4f99
yoggy <yoggy0@gmail.com>
2010-06-18 18:16:28 +0900

    modified:   signup.rb 	modified:   views/signup.erb

--- signup.rb
+++ signup.rb
@@ -10,18 +10,19 @@ end
 post '/signup2' do
   session_clear
 
-  name     = params['name']
-  email    = params['email']
-  password = params['password']
+  name      = params['name']
+  email     = params['email']
+  password  = params['password']
+  password2 = params['password2']
 
-  if name.nil? || email.nil? || password.nil? || name == "" || email == "" || password == ""
+  if name.nil? || email.nil? || password.nil? || name == "" || email == "" || password == "" || password2 == "" || password != password2
     redirect "/signup"
   else
     if User.find_by_name(name).nil? && User.find_by_email(email).nil?
       u = User.new
-      u.name     = params['name']
-      u.email    = params['email']
-      u.password = Digest::SHA1.hexdigest(params['password'])
+      u.name     = name
+      u.email    = email
+      u.password = Digest::SHA1.hexdigest(password)
       u.save
     end
     erb :signup2, :layout => false
